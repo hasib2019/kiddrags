9b578428a2c363dd91c872861beb4a9427ea1ffb
hasib2018 <hasib.9437.hu@gmail.com>
2022-01-19 15:59:37 +0600

    Dialog upload

--- app/src/main/java/com/example/kiddyrags/MainActivity.java
+++ app/src/main/java/com/example/kiddyrags/MainActivity.java
@@ -4,22 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mywebView;
+    private ProgressBar progress_circular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mywebView = (WebView) findViewById(R.id.webview);
+        progress_circular =  findViewById(R.id.progress_circular);
+
+
+
+
+        //mywebView.setVisibility(View.VISIBLE);
+        //progress_circular.setVisibility(View.GONE);
+
+        String url = "https://kiddyrags.com/";
         mywebView.setWebViewClient(new WebViewClient());
+        mywebView.getSettings().setJavaScriptEnabled(true);
+        mywebView.getSettings().setDomStorageEnabled(true);
+        mywebView.loadUrl(url);
+        progress_circular.setVisibility(View.GONE);
+        mywebView.setVisibility(View.VISIBLE);
+
+        /*mywebView.setWebViewClient(new WebViewClient());
         mywebView.loadUrl("https://kiddyrags.com/");
         WebSettings webSettings = mywebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptEnabled(true);*/
     }
 
     public class mywebClient extends WebViewClient {
@@ -31,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
+
             return true;
         }
     }
